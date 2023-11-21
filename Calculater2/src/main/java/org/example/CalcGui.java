664eccf53c16a8459caf48b9956037908248b3ff
Ruby75 <ruby-75@hotmail.com>
2023-11-21 10:32:09 -0400

    added gui to calculator

---
+++ Calculater2/src/main/java/org/example/CalcGui.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import javax.swing.*;
+
+public class CalcGui {
+
+    private JButton clrButton;
+    private JButton a6Button;
+    private JButton a5Button;
+    private JButton a4Button;
+    private JButton button5;
+    private JButton a0Button;
+    private JButton button7;
+    private JButton button8;
+    private JButton a9Button;
+    private JButton a8Button;
+    private JButton a7Button;
+    private JButton button12;
+    private JTextField a1256TextField;
+    private JButton a3Button;
+    private JButton a2Button;
+    private JButton a1Button1;
+    private JButton button16;
+    private JPanel panelMain;
+    private JFrame mainFrame;
+
+
+    public CalcGui() {
+        mainFrame = new JFrame("Calculator");
+        mainFrame.setSize(400, 400);
+        mainFrame.setContentPane(panelMain);
+        mainFrame.setVisible(true);
+    }
+}
---
+++ Calculater2/src/main/java/org/example/Calcutator.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Calcutator {
+    public Calcutator() {
+    }
+}
